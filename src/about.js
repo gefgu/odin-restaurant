f1e9d9c2cbf2183508e1ecddf7497f39c20e8170
gefgu <gefgu@hotmail.com>
2022-02-13 10:27:35 -0300

    Add different dummy text in each tab

--- src/about.js
+++ src/about.js
@@ -9,17 +9,11 @@ export default function aboutComponent() {
   image.alt = "Restaurant Image";
 
   const paragraph = document.createElement("p");
-  paragraph.textContent = `Vivamus elementum libero eu dui accumsan, sed dapibus lorem venenatis.
-  Nunc sagittis sem quis facilisis semper. Integer turpis nunc,
-  sollicitudin sit amet enim eget, viverra tempus neque. Nullam sed mattis
-  turpis, eget blandit diam. Nulla blandit aliquet erat, in suscipit lorem
-  tincidunt at. Nunc ac posuere ligula, in vulputate tellus. Etiam quis
-  rutrum tellus.`;
+  paragraph.textContent = `In et lacus vitae mauris placerat pharetra at at arcu. Etiam lacinia magna in dolor porta feugiat. Maecenas ut metus nibh. Morbi bibendum velit nibh, eu venenatis nibh molestie iaculis. Proin vitae risus risus. Duis accumsan sed nisi tempus ullamcorper. Pellentesque non ante eu lorem vehicula fringilla. Sed ut elit sed velit iaculis mollis. Curabitur feugiat risus vel libero dapibus, eu ornare dui finibus. Nullam finibus neque lobortis justo ultrices malesuada vel et nisi. Donec rutrum eget dolor id sagittis. Nulla elit lorem, faucibus vitae viverra non, tristique vitae purus. Aliquam sed metus hendrerit, sagittis nunc non, tincidunt tellus. Ut nisi lorem, sollicitudin nec nisi quis, fermentum porttitor nulla. Etiam blandit pharetra nisl, eu porta orci aliquet elementum. `;
 
   const container = document.createElement("div");
   container.classList.add("container");
   container.append(heading, image, paragraph);
 
   return container;
-
 }
--- src/menu.js
+++ src/menu.js
@@ -9,17 +9,11 @@ export default function menuComponent() {
   image.alt = "Restaurant Image";
 
   const paragraph = document.createElement("p");
-  paragraph.textContent = `Vivamus elementum libero eu dui accumsan, sed dapibus lorem venenatis.
-  Nunc sagittis sem quis facilisis semper. Integer turpis nunc,
-  sollicitudin sit amet enim eget, viverra tempus neque. Nullam sed mattis
-  turpis, eget blandit diam. Nulla blandit aliquet erat, in suscipit lorem
-  tincidunt at. Nunc ac posuere ligula, in vulputate tellus. Etiam quis
-  rutrum tellus.`;
+  paragraph.textContent = `Morbi pellentesque tortor sed elit ullamcorper lacinia. Aenean placerat at nisl sit amet sodales. Nulla blandit congue est porttitor vulputate. In porta accumsan arcu, a porta sem tincidunt in. Mauris condimentum orci et congue dignissim. Nullam sit amet urna quis nisl finibus commodo. Nunc vitae faucibus ipsum. Ut cursus purus at eros mollis, in consectetur lectus ultrices. In ac diam at nisi sodales dignissim non semper sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam porta leo sit amet nisi pretium, sed eleifend est sagittis. Aenean eget ultricies diam, in ultrices ante. Ut nec bibendum mauris. Etiam velit nisl, gravida a diam et, ultricies commodo erat. Quisque in risus luctus, rutrum velit non, auctor metus. Cras in ultricies tortor. .`;
 
   const container = document.createElement("div");
   container.classList.add("container");
   container.append(heading, image, paragraph);
 
   return container;
-
 }
